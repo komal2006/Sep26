d1075e8745b7395533de2fec2f31efe6b3649e7f
macstudent <komaldeepkr99@gmail.com>
2019-09-26 19:00:16 -0400

    finally

--- src/main/java/com/lambton/ExceptionHandling.java
+++ src/main/java/com/lambton/ExceptionHandling.java
@@ -13,20 +13,30 @@ public class ExceptionHandling
         {
            x[4] = 1000;
             c = a / b;
+            System.out.println("C: " + c);
         }
-       /* catch(ArithmeticException ae)
+
+     catch(ArithmeticException ae)
         {
             System.out.println("value of b =0 , Divisible by zero error");
         }
         catch(ArrayIndexOutOfBoundsException acb)
         {
             System.out.println("Array x is trying access index not available");
-        }*/
+        }
 
         catch(Exception ae)
         {
             System.out.println(ae.toString());
         }
+        finally
+        {
+            System.out.println("i am in finally block");
+        }
+
+
+
+
         System.out.println("END");
     }
 }
